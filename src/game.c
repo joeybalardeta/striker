fa9f63000173e574a6e898a0c99bb4c8c424e7a7
Joey Balardeta <jgbalardeta@gmail.com>
2024-07-01 12:08:55 -0700

    Added game.h/game.c

---
+++ src/game.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "game.h"
+
+
+Game *create_game() {
+	Game *game = malloc(sizeof(Game));
+	game->player_w = 0;
+	game->player_b = 0;
+	
+	game->move = 0;
+	game->castle_kingside_w = 1;
+	game->castle_queenside_w = 1;
+	game->castle_kingside_b = 1;
+	game->castle_queenside_b = 1;
+
+	return game;
+}
+
+
+Game *clone_game(Game *game) {
+	Game *clone = malloc(sizeof(Game));
+	clone->player_w = game->player_w;
+	clone->player_b = game->player_b;
+	clone->move = game->move;
+	clone->castle_kingside_w = game->castle_kingside_w;
+	clone->castle_queenside_w = game->castle_queenside_w;
+	clone->castle_kingside_b = game->castle_kingside_b;
+	clone->castle_queenside_b = game->castle_queenside_b;
+
+	return clone;
+}	
+
+void change_turn(Game *game) {
+	game->move = ~(game->move);
+}
+
+
+void set_player_w(Game *game, uint8_t player_w) {
+	game->player_w = player_w;
+}
+
+
+void set_player_b(Game *game, uint8_t player_b) {
+	game->player_b = player_b;
+}
+
+
+void set_default_board(Game *game) {
+
+
+}
+
+
+void move(Game *game, uint8_t from, uint8_t to) {
+	game->board[to] = game->board[from];
+	game->board[from] = 0x00;
+}
+
---
+++ src/game.h
@@ -0,0 +1,45 @@
+#ifndef GAMEH
+#define GAMEH
+
+#include <stdint.h>
+
+#define BOARD_SIZE 64
+
+
+typedef struct {
+	uint8_t player_w;
+	uint8_t player_b;
+
+	uint8_t board[BOARD_SIZE];
+
+	// game info bit map
+	// bit 0 - move (0 = white, 1 = black)
+	// bit 1 - castling rights (white, kingside) (starts as 1)
+	// bit 2 - castling rights (white, queenside) (starts as 1)
+	// bit 3 - castling rights (black, kingside) (starts as 1)
+	// bit 4 - castling rights (black, queenside) (starts as 1)
+	// bit 5 - none
+	// bit 6 - none
+	// bit 7 - none
+	// uint8_t game_info;
+	uint8_t move : 1;
+	uint8_t castle_kingside_w : 1;
+	uint8_t castle_queenside_w : 1;
+	uint8_t castle_kingside_b : 1;
+	uint8_t castle_queenside_b : 1;
+	uint8_t none : 3;					// extra 3 bit padding to make a full byte	
+
+	uint8_t en_passant_square;			// if larger than 63, invalid, if <= 63, then valid
+} Game;
+
+
+Game *create_game();
+Game *clone_game(Game *game);
+void change_turn(Game *game);
+void set_player_w(Game *game, uint8_t player_w);
+void set_player_b(Game *game, uint8_t player_b);
+void set_default_board(Game *game);
+void move(Game *game, uint8_t from, uint8_t to);
+
+
+#endif
